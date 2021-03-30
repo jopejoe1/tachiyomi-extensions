f56140636bf6f9011882ffb53b289c9aed8e72ba
Johannes Joens <34899572+jopejoe1@users.noreply.github.com>
2021-03-31 00:22:02 +1300

    Added lots of Kakalot copy cats

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/mangabox/MangaBoxGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/mangabox/MangaBoxGenerator.kt
@@ -17,7 +17,14 @@ class MangaBoxGenerator : ThemeSourceGenerator {
         SingleLang("Mangabat", "https://m.mangabat.com", "en", overrideVersionCode = 4),
         SingleLang("Mangakakalots (unoriginal)", "https://mangakakalots.com", "en", className = "Mangakakalots", pkgName = "mangakakalots"),
         SingleLang("Mangairo", "https://h.mangairo.com", "en", overrideVersionCode = 3),
-        SingleLang("Mangakakalot.tv (unoriginal)", "https://ww.mangakakalot.tv", "en", className = "MangakakalotTv", pkgName = "mangakakalotTv"),
+        SingleLang("Mangakakalot.tv (unoriginal)", "https://ww.mangakakalot.tv", "en", className = "MangakakalotTv", pkgName = "mangakakalottv"),
+        SingleLang("Mangakakalotz (unoriginal)", "https://chapter.mangakakalotz.com/", "en", className = "Mangakakalotz", pkgName = "mangakakalotz"),
+        SingleLang("Mangakakalot/Mangarock (unoriginal)", "https://mangarock.herokuapp.com/", "en", className = "MangakakalotMangarock", pkgName = "mangakakalotmangarock"), //url says mangarock title says Mangakakalot
+        SingleLang("Mangakakalot.xyz (unoriginal)", "http://mangakakalot.xyz/", "en", className = "Mangakakalotxyz", pkgName = "mangakakalotxyz"),
+        SingleLang("Mangakakalot.cloud (unoriginal)", "http://mangakakalot.cloud/", "en", className = "MangakakalotCloud", pkgName = "mangakakalotcloud"),
+        SingleLang("Mangakakalot.top (unoriginal)", "http://mangakakalot.top/", "en", className = "MangakakalotTop", pkgName = "mangakakalottop"),
+        SingleLang("Mangakakalot.live (unoriginal)", "http://mangakakalot.live/", "en", className = "MangakakalotLive", pkgName = "mangakakalotlive"),
+        SingleLang("Mangakakalot.city (unoriginal)", "http://mangakakalot.city/", "en", className = "MangakakalotCity", pkgName = "mangakakalotCity"),
     )
 
     companion object {
