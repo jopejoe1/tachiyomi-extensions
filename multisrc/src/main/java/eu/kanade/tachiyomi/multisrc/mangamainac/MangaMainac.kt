87efd527150d9d09417871862a015ce2cc55fe5b
jopejoe1 <johannes@joens.email>
2021-04-01 02:03:56 +1300

    Changed part of the code from TCBScans source to MangaMainac source for imporoved parsing

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/mangamainac/MangaMainac.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/mangamainac/MangaMainac.kt
@@ -13,6 +13,10 @@ import okhttp3.Response
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import java.util.Calendar
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.util.asJsoup
+import rx.Observable
+
 
 // Based On TCBScans sources
 // MangaManiac is a network of sites built by Animemaniac.co.
@@ -57,27 +61,66 @@ abstract class MangaMainac(
     // manga details
     override fun mangaDetailsParse(document: Document) = SManga.create().apply {
         val info = document.select(".intro_content").text()
+        thumbnail_url = document.select(".mangainfo_body > img").attr("src")
         title = document.select(".intro_content h2").text()
         author = if ("Author" in info) substringextract(info, "Author(s):", "Released") else null
         artist = author
         genre = if ("Genre" in info) substringextract(info, "Genre(s):", "Status") else null
-        status = when (substringextract(info, "Status:", "(")) {
-            "Ongoing" -> SManga.ONGOING
-            "Completed" -> SManga.COMPLETED
-            else -> SManga.UNKNOWN
-        }
-        description = if ("Description" in info) info.substringAfter("Description:").trim() else null
-        thumbnail_url = document.select(".mangainfo_body img").attr("src")
+        status = parseStatus(document.select(".intro_content").text())
+        description = if ("Description" in info) info.substringAfter("Description:").trim() else null }
     }
+
     private fun substringextract(text: String, start: String, end: String): String = text.substringAfter(start).substringBefore(end).trim()
 
-    // chapters
-    override fun chapterListSelector(): String = ".chap_tab tr"
-    override fun chapterFromElement(element: Element): SChapter = SChapter.create().apply {
-        name = element.select("a").text()
-        url = element.select("a").attr("abs:href")
-        date_upload = parseRelativeDate(element.select("#time").text().substringBefore(" ago"))
+    private fun parseStatus(element: String): Int = when {
+        element.toLowerCase().contains("ongoing (pub") -> SManga.ONGOING
+        element.toLowerCase().contains("completed (pub") -> SManga.COMPLETED
+        else -> SManga.UNKNOWN
     }
+
+    // chapters
+    override fun chapterListSelector() = "table.chap_tab tr"
+
+    override fun chapterFromElement(element: Element): SChapter {
+        val urlElement = element.select("a").first()
+        val chapter = SChapter.create()
+        chapter.setUrlWithoutDomain(urlElement.attr("href"))
+        chapter.name = element.select("a").text()
+        chapter.date_upload = element.select("#time i").last()?.text()?.let { parseChapterDate(it) }
+            ?: 0
+        return chapter
+    }
+
+    private fun parseChapterDate(date: String): Long {
+        val dateWords: List<String> = date.split(" ")
+        if (dateWords.size == 3) {
+            val timeAgo = Integer.parseInt(dateWords[0])
+            val dates: Calendar = Calendar.getInstance()
+            when {
+                dateWords[1].contains("minute") -> {
+                    dates.add(Calendar.MINUTE, -timeAgo)
+                }
+                dateWords[1].contains("hour") -> {
+                    dates.add(Calendar.HOUR_OF_DAY, -timeAgo)
+                }
+                dateWords[1].contains("day") -> {
+                    dates.add(Calendar.DAY_OF_YEAR, -timeAgo)
+                }
+                dateWords[1].contains("week") -> {
+                    dates.add(Calendar.WEEK_OF_YEAR, -timeAgo)
+                }
+                dateWords[1].contains("month") -> {
+                    dates.add(Calendar.MONTH, -timeAgo)
+                }
+                dateWords[1].contains("year") -> {
+                    dates.add(Calendar.YEAR, -timeAgo)
+                }
+            }
+            return dates.timeInMillis
+        }
+        return 0L
+    }
+
     override fun chapterListParse(response: Response): List<SChapter> {
         val document = response.asJsoup()
         val chapterList = document.select(chapterListSelector()).map { chapterFromElement(it) }
@@ -87,9 +130,13 @@ abstract class MangaMainac(
         else
             chapterList
     }
+
     private fun hasCountdown(chapter: SChapter): Boolean {
         val document = client.newCall(
-            GET(chapter.url, headersBuilder().build())
+            GET(
+                baseUrl + chapter.url,
+                headersBuilder().build()
+            )
         ).execute().asJsoup()
 
         return document
@@ -97,33 +144,20 @@ abstract class MangaMainac(
             .isNotEmpty()
     }
 
-    // Subtract relative date (e.g. posted 3 days ago)
-    private fun parseRelativeDate(date: String): Long {
-        val calendar = Calendar.getInstance()
-
-        if (date.contains("yesterday")) {
-            calendar.apply { add(Calendar.DAY_OF_MONTH, -1) }
-        } else {
-            val trimmedDate = date.replace("one", "1").removeSuffix("s").split(" ")
-
-            when (trimmedDate[1]) {
-                "year" -> calendar.apply { add(Calendar.YEAR, -trimmedDate[0].toInt()) }
-                "month" -> calendar.apply { add(Calendar.MONTH, -trimmedDate[0].toInt()) }
-                "week" -> calendar.apply { add(Calendar.WEEK_OF_MONTH, -trimmedDate[0].toInt()) }
-                "day" -> calendar.apply { add(Calendar.DAY_OF_MONTH, -trimmedDate[0].toInt()) }
+    // pages
+    override fun pageListParse(document: Document): List<Page> {
+        val pages = mutableListOf<Page>()
+        var i = 0
+        document.select(".container .img_container center img").forEach { element ->
+            val url = element.attr("src")
+            i++
+            if (url.isNotEmpty()) {
+                pages.add(Page(i, "", url))
             }
         }
-
-        return calendar.timeInMillis
+        return pages
     }
 
-    // Pages
-
-    override fun pageListParse(document: Document): List<Page> = mutableListOf<Page>().apply {
-        document.select(".img_container img").forEach { img ->
-            add(Page(size, "", img.attr("src")))
-        }
-    }
-
-    override fun imageUrlParse(document: Document): String = throw Exception("Not Used")
+    override fun imageUrlParse(document: Document) = ""
 }
+
