4398b1fd876de8039ced9e40055639a8d747a456
jopejoe1 <johannes@joens.email>
2021-04-02 00:05:10 +1300

    Kalaklot city update fix

--- multisrc/overrides/mangabox/mangakakalotcity/src/MangakakalotCity.kt
+++ multisrc/overrides/mangabox/mangakakalotcity/src/MangakakalotCity.kt
@@ -2,9 +2,21 @@ package eu.kanade.tachiyomi.extension.en.mangakakalotcity
 
 import eu.kanade.tachiyomi.multisrc.mangabox.MangaBox
 import okhttp3.Headers
+import java.util.Calendar
+import java.util.Locale
+import java.util.concurrent.TimeUnit
+import okhttp3.Headers
+import okhttp3.HttpUrl
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 
 class MangakakalotCity : MangaBox("Mangakakalot.city (unoriginal)", "http://mangakakalot.city/", "en") {
     override val simpleQueryPath = "search?q="
     override val popularUrlPath = "popular-manga?page="
     override val latestUrlPath = "latest-manga?page="
+    override val client: OkHttpClient = network.cloudflareClient.newBuilder()
+        .connectTimeout(120, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .build()
 }
