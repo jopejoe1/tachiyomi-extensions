c89b54ebfe0bce572dc7f4c5b6f3c6db65350b18
jopejoe1 <johannes@joens.email>
2021-04-01 23:44:39 +1300

    Kalaklot city update fix

--- multisrc/overrides/mangabox/mangakakalotcity/src/MangakakalotCity.kt
+++ multisrc/overrides/mangabox/mangakakalotcity/src/MangakakalotCity.kt
@@ -4,7 +4,7 @@ import eu.kanade.tachiyomi.multisrc.mangabox.MangaBox
 import okhttp3.Headers
 
 class MangakakalotCity : MangaBox("Mangakakalot.city (unoriginal)", "http://mangakakalot.city/", "en") {
-    override fun simpleQueryPath() = "search?q="
-    override fun popularUrlPath() = "popular-manga?page="
-    override fun latestUrlPath() = "latest-manga?page="
+    override val simpleQueryPath = "search?q="
+    override val popularUrlPath = "popular-manga?page="
+    override val latestUrlPath = "latest-manga?page="
 }
