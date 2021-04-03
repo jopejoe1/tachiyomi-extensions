6c09f331f979f7a699438025395a066685c6ea6b
jopejoe1 <johannes@joens.email>
2021-04-03 14:46:19 +1300

    Fixed typo

--- multisrc/overrides/ww3read/readthepromisedneverlandmangaonline/src/ReadThePromisedNeverlandMangaOnline.kt
+++ multisrc/overrides/ww3read/readthepromisedneverlandmangaonline/src/ReadThePromisedNeverlandMangaOnline.kt
@@ -4,7 +4,7 @@ import eu.kanade.tachiyomi.multisrc.ww3read.Ww3Read
 import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.util.asJsoup
 
-class ReadThePromisedNeverlandMangaOnline : Ww3Read("Read The Promised Neverland Manga Online", "https://ww3.readneverland.com") {
+class ReadThePromisedNeverlandMangaOnline : Ww3Read("Read The Promised Neverland Manga Online", "https://ww3.readneverland.com", "en") {
     override val sourceList = listOf(
         Pair("The Promised Neverland", "$baseUrl/manga/the-promised-neverland/"),
         Pair("Parody", "$baseUrl/manga/the-parodied-jokeland/"),
