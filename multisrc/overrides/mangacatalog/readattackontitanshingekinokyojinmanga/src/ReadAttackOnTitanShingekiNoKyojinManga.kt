034ff990071cd29fc0281d9b8a9fbf5977e5b795
jopejoe1 <johannes@joens.email>
2021-04-04 15:28:46 +1200

    hopluly fix atach on titan

--- multisrc/overrides/mangacatalog/readattackontitanshingekinokyojinmanga/src/ReadAttackOnTitanShingekiNoKyojinManga.kt
+++ multisrc/overrides/mangacatalog/readattackontitanshingekinokyojinmanga/src/ReadAttackOnTitanShingekiNoKyojinManga.kt
@@ -3,8 +3,16 @@ package eu.kanade.tachiyomi.extension.en.readattackontitanshingekinokyojinmanga
 import eu.kanade.tachiyomi.multisrc.mangacatalog.MangaCatalog
 import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.util.asJsoup
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import okhttp3.Request
+import rx.Observable
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
 
 class ReadAttackOnTitanShingekiNoKyojinManga : MangaCatalog("Read Attack on Titan/Shingeki no Kyojin Manga", "https://ww7.readsnk.com", "en") {
     override val sourceList = listOf(
@@ -21,7 +29,9 @@ class ReadAttackOnTitanShingekiNoKyojinManga : MangaCatalog("Read Attack on Tita
         Pair("Guidebook", "$baseUrl/manga/attack-on-titan-guidebook-inside-outside/"),
         Pair("No Regrets Colored", "$baseUrl/manga/attack-on-titan-no-regrets-colored/"),
     ).sortedBy { it.first }.distinctBy { it.second }
+
     override fun chapterListSelector(): String = "div.w-full > .bg-white > .flex"
+
     override fun chapterFromElement(element: Element): SChapter = SChapter.create().apply {
         val name1 = element.select(".flex > a.text-gray-900").text()
         val name2 = element.select(".flex > div.text-xs").text()
