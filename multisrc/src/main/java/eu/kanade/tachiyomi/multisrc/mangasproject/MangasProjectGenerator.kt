f0406a56dbc141cde55ba0518641e8925783bed4
jopejoe1 <johannes@joens.email>
2021-04-07 06:24:31 +1200

    Split MangasProject

---
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/mangasproject/MangasProjectGenerator.kt
@@ -0,0 +1,26 @@
+package eu.kanade.tachiyomi.multisrc.mangasproject
+
+import generator.ThemeSourceData.SingleLang
+import generator.ThemeSourceGenerator
+
+class MangasProjectGenerator : ThemeSourceGenerator {
+
+    override val themePkg = "mangabox"
+
+    override val themeClass = "MangasProject"
+
+    override val baseVersionCode: Int = 1
+
+    override val sources = listOf(
+        SingleLang("Leitor.net", "https://leitor.net", "pt-br", className = "LeitorNet"),
+        SingleLang("Mangá Livre", "https://mangalivre.net", "pt-br", className = "MangaLivre"),
+        SingleLang("Toonei", "https://toonei.com", "pt-br"),
+    )
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            MangasProjectGenerator().createAll()
+        }
+    }
+}
