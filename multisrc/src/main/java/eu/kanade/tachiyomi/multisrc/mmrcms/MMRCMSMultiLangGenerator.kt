1932903c4f6b7055ab3c174d2731e29c9f4bcf6b
jopejoe1 <johannes@joens.email>
2021-04-17 11:02:54 +1200

    First Test of Multisrc MMRCMS

---
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/mmrcms/MMRCMSMultiLangGenerator.kt
@@ -0,0 +1,26 @@
+package eu.kanade.tachiyomi.multisrc.mmrcms
+
+import generator.ThemeSourceData.MultiLang
+import generator.ThemeSourceData.SingleLang
+import generator.ThemeSourceGenerator
+
+class MMRCMSMultiLangGenerator : ThemeSourceGenerator {
+
+    override val themePkg = "mmrcms"
+
+    override val themeClass = "MMRCMSMultiLang"
+
+    override val baseVersionCode: Int = 3
+
+    override val sources = listOf(
+
+        MultiLang("HentaiShark", "https://www.hentaishark.com", listOf("en", "ja", "zh", "de", "nl", "ko", "cz", "eo", "mn", "ar", "sk", "la", "ua", "ceb", "tl", "fi", "bg", "tr"), isNsfw = true, className = "HentaiSharkFactory"),
+    )
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            MMRCMSMultiLangGenerator().createAll()
+        }
+    }
+}
