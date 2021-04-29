5e5e6365162eb2e6066084cb58e29d9b1b949614
johannes joens <jopejoe1.de@gmail.com>
2021-04-30 00:27:51 +1200

    First Try for Bug fix

---
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/mmrcms/SourceData.kt
@@ -0,0 +1,12 @@
+package eu.kanade.tachiyomi.multisrc.mmrcms
+
+
+class SourceData() {
+    companion object {
+        fun giveMetaData(url:String): String{
+            return when (url) {
+                else -> ""
+            }
+        }
+    }
+}
