527e93f51f5b6534f960e6c463d2b2637073e4a3
johannes joens <jopejoe1.de@gmail.com>
2021-05-03 17:51:37 +1200

    Added missing imports

--- multisrc/overrides/mangacatalog/readswordartonlinemangaonline/src/ReadSwordArtOnlineMangaOnline.kt
+++ multisrc/overrides/mangacatalog/readswordartonlinemangaonline/src/ReadSwordArtOnlineMangaOnline.kt
@@ -3,6 +3,16 @@ package eu.kanade.tachiyomi.extension.en.readswordartonlinemangaonline
 import eu.kanade.tachiyomi.multisrc.mangacatalog.MangaCatalog
 import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.util.asJsoup
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.SChapter
+import eu.kanade.tachiyomi.source.model.SManga
+import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import okhttp3.Request
+import rx.Observable
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
 
 class ReadSwordArtOnlineMangaOnline : MangaCatalog("Read Sword Art Online Manga Online", "https://manga.watchsao.tv", "en") {
     override val sourceList = listOf(
