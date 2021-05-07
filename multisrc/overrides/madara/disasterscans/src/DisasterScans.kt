f15ea816c6ae3becbd615f0ea0529cdd31d56f7b
johannes joens <jopejoe1.de@gmail.com>
2021-05-07 18:57:22 +1200

    Added madar soiurces

--- multisrc/overrides/madara/disasterscans/src/DisasterScans.kt
+++ multisrc/overrides/madara/disasterscans/src/DisasterScans.kt
@@ -1,17 +1,9 @@
 package eu.kanade.tachiyomi.extension.en.disasterscans
 
-import android.app.Application
-import android.content.SharedPreferences
-import androidx.preference.ListPreference
-import androidx.preference.PreferenceScreen
 import eu.kanade.tachiyomi.multisrc.madara.Madara
-import eu.kanade.tachiyomi.source.ConfigurableSource
 import eu.kanade.tachiyomi.source.model.SManga
 import org.jsoup.nodes.Document
-import uy.kohesive.injekt.Injekt
-import uy.kohesive.injekt.api.get
-
-class DisasterScans : ConfigurableSource, Madara("Disaster Scans", "https://disasterscans.com", "en") {
+class DisasterScans : Madara("Disaster Scans", "https://disasterscans.com", "en") {
     override val popularMangaUrlSelector = "div.post-title a:last-child"
 
     override fun mangaDetailsParse(document: Document): SManga {
