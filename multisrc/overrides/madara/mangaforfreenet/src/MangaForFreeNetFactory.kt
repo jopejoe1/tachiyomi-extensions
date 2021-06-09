d7c2706f902844f9d44897585555e784e0aebba4
johannes joens <jopejoe1.de@gmail.com>
2021-06-09 15:08:04 +1200

    Hope that fixes them

--- multisrc/overrides/madara/mangaforfreenet/src/MangaForFreeNetFactory.kt
+++ multisrc/overrides/madara/mangaforfreenet/src/MangaForFreeNetFactory.kt
@@ -8,6 +8,7 @@ class MangaForFreeNetFactory : SourceFactory {
     override fun createSources(): List<Source> = listOf(
         MangaForFreeNetEN(),
         MangaForFreeNetKO(),
+        MangaForFreeNetALL(),
     )
 }
 class MangaForFreeNetEN : Madara("MangaForFree.net", "https://mangaforfree.net", "en") {
@@ -16,3 +17,4 @@ class MangaForFreeNetEN : Madara("MangaForFree.net", "https://mangaforfree.net",
 class MangaForFreeNetKO : Madara("MangaForFree.net", "https://mangaforfree.net", "ko") {
     override fun chapterListSelector() = "li.wp-manga-chapter:contains(Raw)"
 }
+class MangaForFreeNetALL : Madara("MangaForFree.net", "https://mangaforfree.net", "all")
