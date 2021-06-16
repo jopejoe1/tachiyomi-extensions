46121302c1c0c4c7a5e5c8d11e48cae3403f7cc2
johannes joens <jopejoe1.de@gmail.com>
2021-06-16 16:41:06 +1200

    Update Madara Date parsing

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
@@ -29,6 +29,11 @@ class MadaraGenerator : ThemeSourceGenerator {
         SingleLang("AllPornComic", "https://allporncomic.com", "en", isNsfw = true),
         SingleLang("Hiperdex", "https://hiperdex2.com", "en", isNsfw = true, overrideVersionCode = 1),
         SingleLang("Mangceh", "https://mangceh.me", "id", isNsfw = true, overrideVersionCode = 1),
+        SingleLang("InfraFandub", "https://infrafandub.xyz", "es"),
+        SingleLang("Hades no Fansub", "https://mangareaderpro.com", "es"),
+        SingleLang("Hades no Fansub H", "https://h.mangareaderpro.com", "es", isNsfw = true),
+        SingleLang("Mangaka3rb", "https://mangaka3rb.com", "ar"),
+        SingleLang("Wekomic", "https://wekomic.com", "en"),
     )
 
     companion object {
